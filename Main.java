4a2a2720425be69e0d7d208e741a2c700761a0df
ErtugrulSuluc <141870278+ErtugrulSuluc@users.noreply.github.com>
2023-08-09 22:21:02 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int yas = 5 ;
+        System.out.println(yas);
+        yas = yas+10;
+        System.out.println(yas);
+
+        System.out.println("----------------------");
+
+        String isim  = " Ertuğrul", soyisim =" Sülüç";
+        System.out.println("İsminiz="+isim+soyisim);
+
+
+        System.out.println("---------------------------------");
+
+        int zaman = 1;
+        System.out.println(zaman++);
+        System.out.println(zaman);
+
+        System.out.println("---------------------------------");
+
+        int yas1 =10;
+        System.out.println(yas1);
+
+        //Kusbakışı baktığımız yeri nasıl sayabiliriz:
+
+        int number1=20;
+        System.out.println("sayı1="+ number1);
+
+
+
+
+
+
+
+
+        
+    }
+}
